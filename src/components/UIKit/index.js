56a21aedaa9d6b4e2215798ea20549dff4af3200
Max Clayton Clowes <maxcc@me.com>
2019-10-20 12:51:54 +0100

    [fix] export of dropdown

--- src/components/UIKit/index.js
+++ src/components/UIKit/index.js
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import Dropdown from "./Dropdown";
 import Icon from "./Icon";
 import Loader from "./Loader";
 import Modal from "./Modal";
@@ -6,4 +7,4 @@ import Padded from "./Padded";
 import Placeholder from "./Placeholder";
 import Text from "./Text";
 
-export { Button, Icon, Loader, Modal, Padded, Placeholder, Text };
+export { Button, Dropdown, Icon, Loader, Modal, Padded, Placeholder, Text };
