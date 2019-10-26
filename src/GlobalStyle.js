a04d9707e4ae831e13a2a40e7895f906e25b7edb
Max Clayton Clowes <maxcc@me.com>
2019-10-26 15:23:37 +0100

    [feat] fade in modal

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components/macro";
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400&display=swap&subset=latin,latin-ext');
 
-  * {
+  *, *:before, *:after {
     box-sizing: border-box;
     font-family: 'Open Sans', sans-serif;
   }
