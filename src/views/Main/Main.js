87b74ce370fe1151240373ed82f8f4fa1ca93b50
Max Clayton Clowes <contact@mcclowes.com>
2020-07-20 19:18:04 +0100

    [feat] react-anime with example on main

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -3,19 +3,28 @@ import React from "react";
 import Smart from "../../components/Smart";
 import { Page, PageWrapper } from "./csx";
 import { Text } from "../../components/UIKit";
+import Anime from "react-anime";
 
 const Main = (props) => {
   return (
     <PageWrapper>
-      <Page>
-        <Smart>
-          <Text>Hello World!</Text>
-        </Smart>
+      <Anime
+        opacity={[0, 1]}
+        translateY={["-4em", 0]}
+        delay={(e, i) => i * 2000}
+      >
+        <Page>
+          <Smart>
+            <Text>Hello World!</Text>
+          </Smart>
 
-        <Dumb>
-          <Text>This is an example project. Make changes as you see fit.</Text>
-        </Dumb>
-      </Page>
+          <Dumb>
+            <Text>
+              This is an example project. Make changes as you see fit.
+            </Text>
+          </Dumb>
+        </Page>
+      </Anime>
     </PageWrapper>
   );
 };
